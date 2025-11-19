a551e32d80cb945bf396d199918a9f3896eeb999
Carvalho, Joao <joao.carvalho@amd.com>
2025-11-19 08:56:53 -0700

    [rocroller] Add changes missed on last gfx1250 update (#445)
    Also adds changes in CI code to allow new lastSuccessfulBuild to be created when one does not exist.

--- shared/rocroller/.jenkins/common.groovy
+++ shared/rocroller/.jenkins/common.groovy
@@ -256,8 +256,12 @@ def runPerformanceCommand (platform, project)
                     cat ./performance_build_${platform.gpu}/performance_${platform.gpu}/**/*.log >> performance_${platform.gpu}_logs.txt
 
                     #Get Master Results
-                    wget ${masterURL}/lastSuccessfulBuild/artifact/*zip*/archive.zip
-                    unzip archive.zip
+                    ARTIFACT_URL_PREFIX="${masterURL}/lastSuccessfulBuild/artifact"
+                    if wget \${ARTIFACT_URL_PREFIX}/*zip*/archive.zip; then
+                      unzip archive.zip
+                    else
+                      echo "WARNING: No lastSuccessfulBuild found at \${ARTIFACT_URL_PREFIX}"
+                    fi
 
                     if [ -f archive/*/*/performance_${platform.gpu}_last.zip ]; then
 
@@ -348,6 +352,15 @@ def runPerformanceCommand (platform, project)
         {
             def ARCHIVE_LIMIT = "101"
 
+            // a parameter can block comparison to target branch
+            def masterCompare = false
+            if (params?."Build target branch for comparison" != null)
+            {
+                masterCompare = params."Build target branch for comparison"
+            }
+
+            String masterCompareString = masterCompare ? "1" : "0"
+
             def command = """#!/usr/bin/env bash
                         set -ex
                         cd ${project.paths.project_build_prefix}/
@@ -355,8 +368,17 @@ def runPerformanceCommand (platform, project)
                         ${sshBlock}
 
                         #Get Master Results
-                        wget ${masterURL}/lastSuccessfulBuild/artifact/*zip*/archive.zip
-                        unzip archive.zip
+                        ARTIFACT_URL_PREFIX="${masterURL}/lastSuccessfulBuild/artifact"
+                        if wget \${ARTIFACT_URL_PREFIX}/*zip*/archive.zip; then
+                          unzip archive.zip
+                        else
+                          if [ "${masterCompareString}" == "1" ]; then
+                            echo "ERROR: No lastSuccessfulBuild found at \${ARTIFACT_URL_PREFIX}"
+                            exit 1
+                          else
+                            echo "WARNING: No lastSuccessfulBuild found at \${ARTIFACT_URL_PREFIX}"
+                          fi
+                        fi
 
                         #Run Performance Test
                         export LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:${project.paths.project_build_prefix}/build/"
--- shared/rocroller/test/unit/MatrixMultiplyTestBase.hpp
+++ shared/rocroller/test/unit/MatrixMultiplyTestBase.hpp
@@ -120,16 +120,24 @@ namespace MatrixMultiplyTest
             if constexpr(isF8<TA> || isF8<TB>)
             {
                 REQUIRE_ANY_OF_ARCH_CAP(GPUCapability::HasMFMA_fp8,
-                                        GPUCapability::HasWMMA_f32_16x16x16_f8);
+                                        GPUCapability::HasWMMA_f32_16x16x16_f8,
+                                        GPUCapability::HasWMMA_f32_16x16x64_f8);
             }
             if constexpr(isF6F4<TA> || isF6F4<TB>)
             {
-                REQUIRE_ANY_OF_ARCH_CAP(GPUCapability::HasMFMA_f8f6f4);
+                REQUIRE_ANY_OF_ARCH_CAP(GPUCapability::HasMFMA_f8f6f4,
+                                        GPUCapability::HasWMMA_f8f6f4);
+            }
+
+            if constexpr(isF32<TA> || isF32<TB>)
+            {
+                REQUIRE_ARCH_CAP(GPUCapability::HasWMMA_f32_16x16x4_f32);
             }
 
             if((isF8<TA> || isF8<TB>)&&(wave_k >= 64))
             {
-                REQUIRE_ANY_OF_ARCH_CAP(GPUCapability::HasMFMA_f8f6f4);
+                REQUIRE_ANY_OF_ARCH_CAP(GPUCapability::HasMFMA_f8f6f4,
+                                        GPUCapability::HasWMMA_f8f6f4);
             }
 
             const bool scaleA         = scaleParams.scaleTypeA != DataType::None;
@@ -140,7 +148,9 @@ namespace MatrixMultiplyTest
 
             if(scaleA || scaleB)
             {
-                REQUIRE_ANY_OF_ARCH_CAP(GPUCapability::HasMFMA_scale_f8f6f4);
+                REQUIRE_ANY_OF_ARCH_CAP(GPUCapability::HasMFMA_scale_f8f6f4,
+                                        GPUCapability::HasWMMA_scale_f8f6f4,
+                                        GPUCapability::HasWMMA_scale16_f8f6f4);
                 const auto& arch = m_context->targetArchitecture();
                 AssertFatal(!scaleA || arch.isSupportedScaleType(scaleTypeA),
                             fmt::format("Scale A set but target {} does not support scale type {}.",
@@ -529,11 +539,16 @@ namespace MatrixMultiplyTest
             if constexpr(isF8<TA> || isF8<TB>)
             {
                 REQUIRE_ANY_OF_ARCH_CAP(GPUCapability::HasMFMA_fp8,
-                                        GPUCapability::HasWMMA_f32_16x16x16_f8);
+                                        GPUCapability::HasWMMA_f32_16x16x16_f8,
+                                        GPUCapability::HasWMMA_f32_16x16x64_f8);
             }
             if constexpr(isF6F4<TA> || isF6F4<TB>)
             {
                 REQUIRE_ARCH_CAP(GPUCapability::HasMFMA_f8f6f4);
+            }
+            if constexpr(isF32<TA> || isF32<TB>)
+            {
+                REQUIRE_ARCH_CAP(GPUCapability::HasWMMA_f32_16x16x4_f32);
             }
 
             auto dataTypeA   = TypeInfo<TA>::Var.dataType;

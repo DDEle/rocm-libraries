8df5ade4ff594970f6c6f3461f9abfbdc98089c0
Huang, Mark <Mark.Huang@amd.com>
2026-02-02 09:04:52 +0800

    add msb comment & fix non-determined msb part (#662)
    Co-authored-by: marhuang_amdeng <marhuang@amd.com>

--- projects/hipblaslt/tensilelite/rocisa/rocisa/include/instruction/instruction.hpp
+++ projects/hipblaslt/tensilelite/rocisa/rocisa/include/instruction/instruction.hpp
@@ -225,20 +225,25 @@ namespace rocisa
                     }
                 }
             }
+            if(!hasVgpr){
+                if(getVgprMsb() == -1)
+                    // HW bug WA: -2 means no-vgpr inst
+                    rocIsa::getInstance().setVgprMsb(-2);
+                return;
+            }
             int newVal = msbSrc[0] + (msbSrc[1] << 2) + (msbSrc[2] << 4) + (msbDst << 6);
             int oriVal = getVgprMsb();
-            if(hasVgpr && newVal != oriVal && !outputInlineAsm){
+            if(newVal != oriVal && !outputInlineAsm){
                 // HW bug WA: need to store previous msb value in [15:8] bits.
-                int setVal;
-                if(oriVal == -1){
-                    // oriVal is non-determined, add snop beforehand
-                    setVal = newVal;
-                    kStr = std::string("s_nop 0\n") + "s_set_vgpr_msb " + std::to_string(setVal) + "\n" + kStr;
-                }
-                else{
-                    setVal = newVal + (oriVal << 8);
-                    kStr = "s_set_vgpr_msb " + std::to_string(setVal) + "\n" + kStr;
-                }
+                int setVal = oriVal < 0? newVal : newVal + (oriVal << 8);
+                std::string msbStr = "s_set_vgpr_msb " + std::to_string(setVal);
+                std::string msbComment = std::string("src0: " + std::to_string(msbSrc[0]) + ", src1: " + std::to_string(msbSrc[1]) + \
+                    ", src2: " + std::to_string(msbSrc[2]) + ", dst: " + std::to_string(msbDst));
+                msbStr = formatStr(false, msbStr, msbComment, false);
+                // HW bug WA: add a no-vgpr inst if oriVal is non-determined and right after label
+                if(oriVal == -1)
+                    msbStr = "s_nop 0\n" + msbStr;
+                kStr = msbStr + kStr;
                 rocIsa::getInstance().setVgprMsb(newVal);
             }
         }

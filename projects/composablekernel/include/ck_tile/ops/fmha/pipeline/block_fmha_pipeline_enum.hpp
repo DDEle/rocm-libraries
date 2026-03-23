ac982130db87e0999fe8ba6d30b311694283f47e
Ding, Yi <yi.ding@amd.com>
2026-03-23 01:57:54 -0500

    Init QRKSVS_TDM

--- projects/composablekernel/include/ck_tile/ops/fmha/pipeline/block_fmha_pipeline_enum.hpp
+++ projects/composablekernel/include/ck_tile/ops/fmha/pipeline/block_fmha_pipeline_enum.hpp
@@ -13,6 +13,7 @@ enum class BlockFmhaPipelineEnum
     QSKSVS,
     QRKSVS_ASYNC_TRLOAD,
     QRKSVS_ASYNC_TRLOAD_V3,
+    QRKSVS_TDM,
 };
 
 template <BlockFmhaPipelineEnum>
@@ -38,6 +39,12 @@ template <>
 struct BlockFmhaPipelineEnumToStr<BlockFmhaPipelineEnum::QRKSVS_ASYNC_TRLOAD>
 {
     static constexpr const char* name = "qr_async_trload";
+};
+
+template <>
+struct BlockFmhaPipelineEnumToStr<BlockFmhaPipelineEnum::QRKSVS_TDM>
+{
+    static constexpr const char* name = "qr_tdm";
 };
 
 } // namespace ck_tile

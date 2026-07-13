89c93dbac4707dfebdb4120bf76df207b0482233
Ding, Yi <yi.ding@amd.com>
2026-07-13 10:49:46 -0400

    fix(tensilelite): align recv N-wide store vPack to 64-bit for dwordx2

--- projects/hipblaslt/tensilelite/Tensile/Components/GlobalWriteBatch.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/GlobalWriteBatch.py
@@ -2896,7 +2896,7 @@ class GlobalWriteBatchWriter:
     vRecvAddr = kw.vgprPool.checkOut(1, tag="fusedA2A_recvAddr")
     vNLocal   = kw.vgprPool.checkOut(1, tag="fusedA2A_nLocal")
     vTmp      = kw.vgprPool.checkOut(1, tag="fusedA2A_tmp")
-    vPack     = kw.vgprPool.checkOut(2, tag="fusedA2A_pack")  # 2 dwords = 4 16bit N-cols
+    vPack     = kw.vgprPool.checkOutAligned(2, 2, tag="fusedA2A_pack")  # 2 dwords, 64-bit aligned for dwordx2
 
     coord0 = kw.vgprs.coord0
     coord1 = kw.vgprs.coord1
